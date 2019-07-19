e24ab5a291d0aa0199629d7fa624f3e5557e90be
shjang <cbiscuit@naver.com>
2019-07-19 18:27:00 +0900

    ictis 18:26

---
+++ Class_example/Class_example/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_example
+{
+    class MyClass
+    {
+        public int MyField1;
+        public int MyField2;
+
+        public MyClass DeepCopy()
+        {
+            MyClass newCopy = new MyClass();
+            newCopy.MyField1 = this.MyField1;
+            newCopy.MyField2 = this.MyField2;
+            return newCopy;
+        }
+    }
+}
---
+++ Class_example/Class_example/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+
+            Console.WriteLine("Shallow Copy");
+            {
+                MyClass source = new MyClass();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
+
+                MyClass target = source;
+                target.MyField2 = 30;
+
+                Console.WriteLine($"{source.MyField1} {source.MyField2}");
+                Console.WriteLine($"{target.MyField1} {target.MyField2}");
+            }
+            Console.WriteLine("Deep Copy");
+
+            {
+                MyClass source = new MyClass();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
+
+                MyClass target = source.DeepCopy();
+                target.MyField2 = 30;
+
+                MyClass target2 = source.DeepCopy();
+                target2.MyField2 = 40;
+
+                Console.WriteLine($"{source.GetHashCode()} {source.MyField2}");
+                Console.WriteLine($"{target.GetHashCode()} {target.MyField2}");
+                Console.WriteLine($"{target2.GetHashCode()} {target2.MyField2}");
+            }
+
+
+        }
+    }
+}
